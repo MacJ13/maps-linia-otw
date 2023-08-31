8563d358c87fafabb86c1c0735278137e130462d
MacJ13 <jacdim02@gmail.com>
2023-08-31 21:52:40 +0200

    Create home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Home from "./pages/home";
+
 function App() {
   return (
     <>
-      <div>Map - linia otwocks</div>
+      <div>
+        <Home />
+      </div>
     </>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div id="landing-page">
+      <div className="intro">
+        <h1 className="h1"> Mapy - Linia Otwocka</h1>
+        <p className="paragraph">
+          Projekt ma na celu przedstawienie oraz zachowanie pamięci o historii
+          różnych miejsc oraz obiektów znajdujących się na terenie linii
+          otwockiej, a także jej okolicach.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
