7e6b148f06ce2391dfe3583c167bc9a1b732e6af
MacJ13 <jacdim02@gmail.com>
2023-08-31 23:19:18 +0200

    Create navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => (
+  <nav id="navbar">
+    <ul id="navbar-list">
+      <a href="#">Główna</a>
+      <a href="#">Mapa</a>
+      <a href="#">O Projekcie</a>
+      <a href="#">Miejsca</a>
+      <a href="#">Kontakt</a>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
