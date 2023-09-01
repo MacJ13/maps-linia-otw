14fbb53c8f35308efca2d60f94b5b4a8bb66392b
MacJ13 <jacdim02@gmail.com>
2023-09-01 18:11:55 +0200

    Add Navlink in Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,29 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => (
-  <nav id="navbar">
-    <ul id="navbar-list">
-      <li>
-        <Link to="/">Główna</Link>
-      </li>
-      <li>
-        <Link to="map">Mapa</Link>
-      </li>
+const Navbar = () => {
+  const activeLink = ({ isActive, isPending }) =>
+    isPending ? "pending" : isActive ? "active" : "";
 
-      <a href="#">O Projekcie</a>
-      <a href="#">Miejsca</a>
-      <a href="#">Kontakt</a>
-    </ul>
-  </nav>
-);
+  return (
+    <nav id="navbar">
+      <ul id="navbar-list">
+        <li>
+          <NavLink to="/" className={activeLink}>
+            Główna
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={activeLink} to="map">
+            Mapa
+          </NavLink>
+        </li>
+
+        <a href="#">O Projekcie</a>
+        <a href="#">Miejsca</a>
+        <a href="#">Kontakt</a>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
